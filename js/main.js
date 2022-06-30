b6f3b607e0a11ba5eccce108a9d23957280dbb3d
arturr92 <gonart.92@gmail.com>
2022-06-29 23:09:59 -0300

    complementario

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
+class Producto {
+  constructor(marca, precio) {
+    this.marca = marca;
+    this.precio = precio;
+  }
+} 
+
+let precios = [
+  {nombre: 'iPhone_11', precio: 206000},
+  {nombre: 'iPhone_12', precio: 263000},
+  {nombre: 'iPhone_13_proMax', precio: 353000},
+]; 
+
 let menu = '';
 
 let nombre = prompt('Cual es su nombre?');
 let producto;
 
-let iPhone_11 = 206000;
-let iPhone_12 = 263000;
-let iPhone_13_proMax = 353000;
-
-let carrito = '';
+let carrito = [];
 let total = 0;
+
 
 function comprarCelular() {
   function saludo() {
@@ -27,25 +37,25 @@ function comprarCelular() {
   function mostrarMenu() {
       do {
         producto = parseInt(prompt('elija uno de nuestros productos \n1) iPhone 11 \n2) iPhone 12 \n3) iPhone 13 Pro Max'));
-  
+
         switch (producto) {
           case 1:
-            total += iPhone_11;
-            carrito += "\n iPhone 11 : $206.000";
-            alert('Se agrego: iPhone 11 al carrito su valor es de $206.000');
+            total += precios[0];
+            carrito.push(new Producto('iPhone 11', 206000));
+            alert(`Se agrego: iPhone 11 al carrito su valor es de $${precios[0].precio}`);
             break;
           case 2:
-            total += iPhone_12;
-            carrito += "\n iPhone 12 : $263.000";
-            alert('Se agrego: iPhone 12 al carrito su valor es de $263.000');
+            total += precios[1];
+            carrito.push(new Producto('iPhone 12', 263000));
+            alert(`Se agrego: iPhone 12 al carrito su valor es de $${precios[1].precio}`);
             break;
           case 3:
-            total += iPhone_13_proMax;
-            carrito += "\n iPhone 13 : $353.000";
-            alert('Se agrego: iPhone 13 Pro Max al carrito su valor es de 353.000');
+            total += precios[2];
+            carrito.push(new Producto('iPhone 13 Pro Max', 353000));
+            alert(`Se agrego: iPhone 13 Pro Max al carrito su valor es de $${precios[2].precio}`);
             break;
         }
-      } while (producto !== 1 && producto !== 2 && producto !== 3 && producto !== 4);
+      } while (producto !== 1 && producto !== 2 && producto !== 3);
   
     opcion = parseInt(prompt('Vuelva a ingresar una opcion \n1) comprar otro celular \n2) finalizar compra'));
   
@@ -57,11 +67,11 @@ function comprarCelular() {
   }
   
   function finalizarCompra() {
-    let mostrarCarrito = carrito;
-    let precioFinal = total;
-    
-    alert(`Usted tiene estos productos en su carrito ${mostrarCarrito}`);
-    alert(`Y el total de su compra es de $${precioFinal}`)
+    carrito.forEach(producto => {
+      alert(`Usted tiene estos productos en su carrito ${producto.marca} $${producto.precio}`);
+    });
+    let precioFinal = carrito.reduce((acc, producto) => acc + producto.precio, 0);
+    alert(`El total de su compra es de $${precioFinal}`)
     alert('Gracias por su compra, vuelva pronto!!');
   } 
   saludo();
@@ -69,7 +79,7 @@ function comprarCelular() {
 
 document.querySelector('#btn').onclick = () => {
   nombre = prompt('Cual es su nombre?');
-  carrito = '';
+  carrito = [];
   total = 0;
 
   comprarCelular();
