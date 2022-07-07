685d8e45b312cd9126e996da5da454f8f9c03fad
arturr92 <gonart.92@gmail.com>
2022-07-06 21:44:19 -0300

    update

---
+++ js/cards.js
@@ -0,0 +1,35 @@
+let cards = document.querySelector('.cards');
+
+let cardsItem = [
+    {
+        nombre: 'iPhone 11',
+        precio: 206000,
+        img: './assets/img/iPhone-11.jpg'
+    },
+    {
+        nombre: 'iPhone 12',
+        precio: 263000,
+        img: './assets/img/iPhone-12.jpg'
+    },
+    {
+        nombre: 'iPhone 13 Pro Max',
+        precio: 353000,
+        img: './assets/img/iPhone-13-promax.jpg'
+    }
+]
+
+let crearCards = () => {
+    cards.innerHTML = cardsItem.map( card => {
+        return (
+            `
+            <div class="card">
+                <h3>${card.nombre}</h3>
+                <p>$${card.precio}</p>
+                <img src="${card.img}">
+            </div>
+        `
+        )
+    });
+};
+
+console.log(crearCards());
